5c56f07e526d215de1847547e9bc45743fd67d6d
Agata Canert <agata.canert@gmail.com>
2017-04-01 14:31:58 +0200

    add slider and js

--- js/app.js
+++ js/app.js
@@ -14,45 +14,10 @@ var canvas = new fabric.Canvas('canvas');
 //   resizeCanvas();
 
 
-// funkcja odpwiedzialna za nadawanie wpełnienia z colorpickera
-function setImageColor(element) {
-
-	var activeObject = canvas.getActiveObject(),
-  		color = "#"+element.value;
-
-  		
-   console.log(color)    
- console.log(activeObject)
-	activeObject.set({fill : color});
-  canvas.renderAll();
-}
-
-// funkcja odpwiedzialna za nadawanie wpełnienia z colorpickera
-function setStrokeColor(element) {
-
-	
-  		color = "#"+element.value;
-  		var obj = canvas.getActiveObject();
-  		var src = obj.src;
-  		console.log(src);
-  		fabric.loadSVGFromURL( src , function(objects, options) {
-	  for(i in objects) {
-	  objects[i].set({
-	    strokeWidth: 8,
-	    stroke: color
-	  });
-	}
-});
-		
-
-		// obj.stroke = color;
-		// obj.strokeWidth = 10; 
-		// console.log(obj);
-  		canvas.renderAll();
-}
 
 
 document.addEventListener('DOMContentLoaded',function(){
+
 
 var shapes =Array.from(document.querySelectorAll(".shapes div img"));
 console.log(shapes);
@@ -65,12 +30,11 @@ e.addEventListener("click", function(){
 fabric.loadSVGFromURL( src, function(objects, options) {
  var obj = fabric.util.groupSVGElements(objects, options);
  
- //console.log(typeof(jscolor));
- //obj.set({  fill: jscolor });
+ 
  canvas.add(obj).renderAll();
- obj.scaleToHeight(127) // Scales it down to some small size
+ obj.scaleToHeight(127) 
         .scaleToWidth(90)
-        .center() // Centers it (no s**t, Sherlock)
+        .center() 
         .setCoords();
  canvas.setActiveObject(obj).renderAll();
 });
@@ -78,7 +42,19 @@ fabric.loadSVGFromURL( src, function(objects, options) {
 });
 
 
+// funkcja odpwiedzialna za nadawanie wpełnienia z colorpickera
 
+function setImageColor(element) {
+
+	var activeObject = canvas.getActiveObject(),
+  		color = "#"+element.value;
+
+  		
+   console.log(color)    
+ console.log(activeObject)
+	activeObject.set({fill : color});
+  canvas.renderAll();
+}
 
 var jscolor = document.getElementById("color");
 console.log(jscolor)
@@ -89,6 +65,28 @@ jscolor.addEventListener('change', function()
     setImageColor(this);
 });
 
+
+// funkcja odpwiedzialna za nadawanie obrysu z colorpickera
+function setStrokeColor(element) {
+  		color = "#"+element.value;
+  		var object = canvas.getActiveObject();
+
+
+  		console.log(color)    
+ console.log(object)
+ if (object.paths) {
+    for (var i = 0; i < object.paths.length; i++) {
+      object.paths[i].set({
+        
+        stroke: color,
+        strokeWidth: 0.5
+      });
+    }
+  }
+	 
+  	canvas.renderAll();
+	}
+
 // wywołanie funkcji odpowiedzlanej za obrys elementu 
 var stroke = document.getElementById("stroke");
 console.log(stroke)
@@ -98,6 +96,12 @@ stroke.addEventListener('change', function()
 	
     setStrokeColor(this);
 });
+
+
+
+
+var strokeWidth =document.getElementById("width").value;
+console.log(strokeWidth);
 
 
 
@@ -119,7 +123,7 @@ stroke.addEventListener('change', function()
 	var reflectH = document.getElementById("reflectH");
 	reflectH.addEventListener("click", function(){
 		var object = canvas.getActiveObject();
-		object.set("angle", "-180").set('flipY', true).center();
+		object.set('flipY', true).center();
 		canvas.renderAll();
 	});
 
@@ -127,7 +131,7 @@ stroke.addEventListener('change', function()
 	var reflectV = document.getElementById("reflectV");
 	reflectV.addEventListener("click", function(){
 		var object = canvas.getActiveObject();
-		object.set("angle", "-180").set('flipX', true).center();
+		object.set('flipX', true).center();
 		canvas.renderAll();
 	});
 
@@ -179,8 +183,15 @@ stroke.addEventListener('change', function()
 				  canvas.add(obj).renderAll();
 
 		});
-	
 
+		// var img = Array.from(document.querySelectorAll(".shapes div img"));
+		// console.log(img);
+
+		// img.forEach(function(e){
+		// 	e.addEventListener("mousemove", function(){
+		// 		this.style.padding = "17px";
+		// 	});
+		// });
 
 });
 
---
+++ js/slider.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+  $('.basic_shapes').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+ 
+});
